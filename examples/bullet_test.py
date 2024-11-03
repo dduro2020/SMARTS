c53f7908a9571fc9fb35f751e0e61c7a0810d091
duro <davidduro2002@gmail.com>
2024-11-03 19:03:29 +0100

    Created bullet test

--- examples/bullet_test.py
+++ examples/bullet_test.py
@@ -1,25 +1,42 @@
 
-import pybullet as p  # Importar PyBullet
+import pybullet as p
+import pybullet_data
+import os, inspect
 
-# Inicia el simulador en modo GUI
+import time
+from smarts.core.lidar_sensor_params import BasicLidar
+from smarts.core.lidar import Lidar
+
+ray_from = [0, 0, 1]
+ray_to = [0, 0, 0]
+
+def use_ray():
+    return p.rayTest(ray_from, ray_to)
+
 p.connect(p.GUI)
 p.resetSimulation()
+p.setGravity(0, 0, -9.8)
+origin = (0,0,0.5)
+lidar = Lidar(origin, BasicLidar)
 
-# Carga una escena básica
+p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
+cube_id = p.loadURDF(os.path.join(pybullet_data.getDataPath(),"r2d2.urdf"), [3, 1, 0.1])
+
+n = 0
 try:
-    # Asegúrate de que los archivos URDF estén en el directorio actual o en la ruta de pybullet
-    p.setAdditionalSearchPath(".")  # Directorio actual
-    # p.loadURDF("plane.urdf")
-    # cube_id = p.loadURDF("r2d2.urdf", [0, 0, 0.5])  # Añadir cubo
+    while True:
+        point_cloud, hits, rays = lidar.compute_point_cloud(bullet_client=p)
 
-    # Prueba de rayos en PyBullet estándar
-    ray_from = [0, 0, 1]
-    ray_to = [0, 0, 0]
-    result = p.rayTest(ray_from, ray_to)
+        # Dibujar cada rayo en PyBullet
+        if n == 0:
+            for i, (start, end) in enumerate(rays):
+                n = 1
+                color = [1, 0, 0] if hits[i] else [0, 1, 0]  # Rojo si impacta, verde si no
+                p.addUserDebugLine(start, end, lineColorRGB=color, lineWidth=1.5)
 
-    print("Ray test result in standard pybullet:", result)
-except Exception as e:
-    print("Error loading URDF files or running ray test:", e)
-finally:
-    # Desconectar después de la ejecución
+        p.stepSimulation()
+        time.sleep(1 / 240)
+except KeyboardInterrupt:
+    print("\nSimulación detenida por el usuario.")
     p.disconnect()
+    
